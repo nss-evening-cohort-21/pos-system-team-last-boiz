177ee9c63956ff85ff4b7ac2a150bca0a80bf204
Claudio Villalobos <claudiodvillalobos@gmail.com>
2022-12-16 15:15:28 -0600

    Adding logo to login page and navbar

--- components/loginButton.js
+++ components/loginButton.js
@@ -2,7 +2,7 @@ import { signIn } from '../utils/auth';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  const domString = '<h1>Welcome, user!</h1></br><button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
+  const domString = '<h1>Welcome to Hip Hop, Pizza, and Wangs!</h1><img id="logo" src="https://user-images.githubusercontent.com/29741570/205346767-a182560c-64a6-4cfa-80b3-0d64cf998242.png" alt="Avatar"></br><button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
   document.querySelector('#app').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
