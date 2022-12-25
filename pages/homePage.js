2c39bfaba52653998e8e16c9a30b3c8ae9a574da
Tyler Eisen <jteisen90@gmail.com>
2022-12-25 12:56:16 -0500

    homepage styled

--- pages/homePage.js
+++ pages/homePage.js
@@ -2,7 +2,7 @@ import renderToDOM from '../utils/renderToDom';
 
 const showHomePage = () => {
   const domString = `
-  <div class="d-grid gap-2">
+  <div class="btn">
     <button class="dom-btn" type="button" id="view-order-btn">View Orders</button>
     <button class="dom-btn" type="button" id="create-order-btn">Create Orders</button>
     <button class="dom-btn" type="button" id="view-revenue-btn">View Revenue</button>
