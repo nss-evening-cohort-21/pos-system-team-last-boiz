52ca91d434f8af6229cb320850944a19d7008bf3
Claudio Villalobos <claudiodvillalobos@gmail.com>
2023-01-01 19:56:36 -0600

    Fixing user id stuff and getting username to show up on page

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,7 +1,8 @@
 import renderToDOM from '../utils/renderToDom';
 
-const showHomePage = () => {
+const showHomePage = (user) => {
   const domString = `
+  <h1>Welcome, ${user.displayName}</h1>
   <div class="btn">
     <button class="dom-btn" type="button" id="view-order-btn">View Orders</button>
     <button class="dom-btn" type="button" id="create-order-btn">Create Orders</button>
