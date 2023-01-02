9a2892e4dffeba0fa47eab083496d526480fdb0c
Claudio Villalobos <claudiodvillalobos@gmail.com>
2023-01-01 20:20:45 -0600

    Adding uid to revenue page

--- pages/revenuePage.js
+++ pages/revenuePage.js
@@ -5,7 +5,7 @@ const revenuePage = (obj = {}) => {
   clearDom();
   console.warn(obj);
   const domString = ` 
-  <h1>Here's our revenue</h1> <i class="fa-solid fa-wave-pulse"></i>
+  <h1>Here's your revenue</h1> <i class="fa-solid fa-wave-pulse"></i>
   <div id="revenue">
   <h1>Total Revenue: $${obj}</h1>
   </div>
